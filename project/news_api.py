32b720114f130bfee980415bf2007a4c428ef5ad
Andy <115085994+VrMoore@users.noreply.github.com>
2024-12-04 17:15:55 +0700

    The Bug Has been Fixed
    The bug from cache writing has been fixed. Please manually add news.json in the same folder. I'll provided more details and changes in next commit

--- project/news_api.py
+++ project/news_api.py
@@ -4,17 +4,21 @@ import hashlib
 import json
 from dotenv import load_dotenv
 
+# Make .env file in the same folder and place your API keys
 load_dotenv()
 api_key = getenv('API_KEY')
 
+# Customize your own parameters, check the link below for more detailed information.
+# https://newsapi.org/docs/endpoints/everything
 parameters = {
     'apiKey' : api_key,
     'country' : 'us',
-    'category' : 'science',
+    'category' : 'health',
     'language' : 'en',
     'sortBy' : 'popularity'
 }
 
+# Load parameter into request
 url = "https://newsapi.org/v2/top-headlines"
 response = get_url(url, params=parameters)
 
@@ -24,43 +28,52 @@ class newsAggr() :
         self.news_list = []
         self.cache_news_method = cacheNews()
 
-    def get_news(self, news_data : dict) :
+    def get_news(self, news_data : dict) -> None :
+        """
+            Check if key is present by calling load_cache method,
+            If TRUE then load the cache,
+            If FALSE then make a request and load it in the cache with new key
+        """
         
         articles = news_data.get('articles')
-
-        # Generate key dulu
-        # Lalu panggil cache_load
-        # Jika ada maka buka news.json
 
         news_key = self.cache_news_method.create_keys(params=parameters, urls=url)
         cache_news_data = self.cache_news_method.load_cache(news_key)
 
-        if cache_news_data is not None :
+        if cache_news_data is True :
 
             with open('api project/news.json',mode='r') as bar :
                 bar_news = json.load(bar)
                 print('Fetching from local....')
                 print(bar_news['article'])
-                
-        for article in articles :
-            news_dict = {
-                'source': article.get('source', {}).get('name', ''),
-                'author': article.get('author', ''),
-                'title': article.get('title', ''),
-                'description': article.get('description', '')
-            }
-            self.news_list.append(news_dict)
-            self.print_output(news=news_dict)
-            
-        return self.news_list
 
-    def fetch_news(self) :
+        elif cache_news_data is False :    
+
+            for article in articles :
+                news_dict = {
+                    'source': article.get('source', {}).get('name', ''),
+                    'author': article.get('author', ''),
+                    'title': article.get('title', ''),
+                    'description': article.get('description', '')
+                }
+                self.news_list.append(news_dict)
+                self.print_output(news=news_dict)
+
+            self.cache_news_method.save_cache(self.news_list)
+                
+        return 
+
+    def fetch_news(self) -> None :
+        """
+            Fetch the news based on request.status_code
+            If status is 200 <SUCCESS> then call get_news method
+        """
         
         if response.status_code == 200 :
             news_data = response.json()
             self.get_news(news_data)
-            self.write_to_local(self.news_list)
-            self.cache_news_method.save_cache(self.news_list)
+            # self.write_to_local(self.news_list)
+            # self.cache_news_method.save_cache(self.news_list)
 
         else :
             return f"{response.status_code}\nTry again laters"
@@ -81,14 +94,20 @@ class cacheNews() :
     def __init__(self) :
         self.cache = {}
 
-    def create_keys(self, params : dict, urls : str) :
+    def create_keys(self, params : dict, urls : str) -> dict:
+        """
+            Create unique key based of parameters, customize your own parameter in global varible
+        """
 
         news_data = hashlib.md5(json.dumps(params, sort_keys=True).encode()).hexdigest()
         self.cache[urls] = news_data
 
         return self.cache
 
-    def save_cache(self, news_article : dict) :
+    def save_cache(self, news_article : dict) -> None:
+        """
+            Save the cache to the cache file with unique key and news article
+        """
 
         self.create_keys(params=parameters, urls=url)
         self.cache['article'] = news_article
@@ -96,19 +115,18 @@ class cacheNews() :
         with open('api project/news.json', mode='w') as cn :
             json.dump(self.cache, cn, indent=4)
 
-    def load_cache(self, token : dict) :
+    def load_cache(self, token : dict) -> bool :
+        """
+            Check cache file whether the key is present or not , return Boolean
+        """
         
         with open(file='api project/news.json', mode='r') as foo :
             open_cache = json.load(foo)
             if token[url] == open_cache[url]:
-                return open_cache['article']
+                return True
             else :
                 return False
 
 if __name__  == "__main__" :
     my_news = newsAggr()
     my_news.fetch_news()
-
-    # my_cache = cacheNews()
-    # my_cache.create_keys(params=parameters, urls=url)
-    # my_cache.load_cache()
