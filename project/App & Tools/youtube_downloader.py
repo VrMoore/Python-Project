01bb7dc62a5a0967d333eb110d6629c82ae31fd8
Andy <115085994+VrMoore@users.noreply.github.com>
2025-03-03 20:51:28 +0700

    updated youtube_downloader
    Fix namespace, user interface, caching and download

--- project/App & Tools/youtube_downloader.py
+++ project/App & Tools/youtube_downloader.py
@@ -1,24 +1,26 @@
-from yt_dlp import YoutubeDL
 import os
 import json
+from yt_dlp import YoutubeDL
 
 # Change your download path
 MAIN_PATH = os.getcwd()
-print(MAIN_PATH)
 
 class youtubeVideo() :
     
     def __init__(self) :
         self.metadata_youtube = showInfo()
         self.file_path = (f"{MAIN_PATH}/downloaded")
-
-    os.makedirs(f"{MAIN_PATH}/downloaded", exist_ok=True)
+        os.makedirs(f"{MAIN_PATH}/downloaded", exist_ok=True) # Immediately create downloaded folder if doesn't exist
 
     def downloadVideos(self, urls : str) :
+        """
+            Download Videos, Audios, or even Subtitles with given url. Automatically saved in /downloader folder
+        """
 
         self.options = {
             'format' :'best',
-            'outtmpl' : os.path.join(f"{self.file_path}",'%(title)s.%(ext)s')
+            'quiet' : True,
+            'outtmpl' : os.path.join(self.file_path, '%(title)s.%(ext)s')
         }
 
         with YoutubeDL(self.options) as yt :
@@ -32,7 +34,7 @@ class youtubeVideo() :
             'extract_audio' : True,
             'audio_format' : 'mp3',
             'audio_quality' : '192',
-            'outtmpl' : os.path.join(f"{self.file_path}",'%(title)s.%(ext)s')
+            'outtmpl' : os.path.join(self.file_path, '%(title)s.%(ext)s')
         }
 
         with YoutubeDL(self.options) as yt :
@@ -48,7 +50,7 @@ class youtubeVideo() :
             'skip_download' : True,
             'quiet' : True,
             'substitlesformat' : 'srt',
-            'outtmpl' : os.path.join(f"{self.file_path}", '%(title)s.%(ext)s')
+            'outtmpl' : os.path.join(self.file_path, '%(title)s.%(ext)s')
         }
 
         with YoutubeDL(self.options) as yt :
@@ -63,16 +65,19 @@ class showInfo() :
             'extract_flat' : True,
             'Force_generic_extractor' : True
         }
+        os.makedirs(f"{MAIN_PATH}/cache", exist_ok=True) # Immediately create cache folder if doesn't exist
 
-    def getMetaData(self, urls : str) :
+    def getMetaData(self, urls : str) -> None:
+        """
+            Extract metadata from given url and save the cache in dump.json.
+            Return None.
+        """
         with YoutubeDL(self.options) as yt :
             data = yt.extract_info(url=urls, download=False)
             self.printMetaData(data=data)
 
-        # Immediately create cache folder if does not exist
-        os.makedirs(f"{MAIN_PATH}/cache", exist_ok=True)
 
-        # youtube_video : dict, only store some of data from metadata
+        # youtube_video : dict, only store some of the data from metadata
         youtube_video = {
                 'id' : data.get('id'),
                 'channel' : data.get('channel'),
@@ -108,12 +113,12 @@ class showInfo() :
                 cache_data.append(youtube_video)
 
 
-                save_youtube_cache = myCache()
+                save_youtube_cache = cacheManager()
                 save_youtube_cache.saveCache(youtube_cache_data=cache_data, cache_video=youtube_video)
             
     def printMetaData(self, data : dict) : 
         """
-            Take an argument from metadata, and print it on the terminal.
+            Take an argument from metadata, and print it on the terminal. Called by showInfo().\n
             Print (id, channel, fulltitle, description). You may modify print statement as well as youtube_video (video's metadata).
         """
         print("=" * 40)
@@ -123,12 +128,15 @@ class showInfo() :
         print(f'Description : \n{data.get("description")}')
         print("=" * 40)
 
-class myCache() :
+class cacheManager() :
+    """
+        Manage all cache operations. From display, looking spesific video even delete cache.
+    """
 
     def __init__(self) :
         self.CACHE_PATH = f"{MAIN_PATH}/cache/dump.json"
 
-    def doExist(self) -> bool : 
+    def doesExist(self) -> bool : 
         """
             Check whether dump.json containt data or not. Return boolean.
         """
@@ -159,26 +167,47 @@ class myCache() :
         """
             look an id from youtube_video (video's metadata) to search it in existing json data.
         """
-        do_ext = self.doExist()
-        if self.doExist() == False :
+
+        # Check wheter file does exist or not
+        if self.doesExist() is False :
             print('No data shown')
             exit()
+
+        # Use r+ mode to read data.
+        with open(self.CACHE_PATH, mode='r+') as file :
+            youtube_cache = json.load(file)
+
+            list_video : list = [x for x in youtube_cache if x['id'] == videos_id]
+
+            if len(list_video) == 0 :
+                print(f'There is no {videos_id} found -------')
+                return False
+            
+            dict_video : dict = list_video[0]
+
+            if videos_id == dict_video.get('id') :
+                print(f'``````Found {videos_id} on cache.``````')
+                return True
+        
+    def lookId(self, videos_id : str) :
+        """
+            Look for spesific videos id and print to the terminal
+        """
+
+        # Check wheter file does exist or not
+        if self.doesExist() is False :
+            return print('No data shown')
 
         with open(self.CACHE_PATH, mode='r') as file :
             youtube_cache = json.load(file)
 
-        list_video : list = [x for x in youtube_cache if x['id'] == videos_id]
+        list_video = next((x for x in youtube_cache if x['id'] == videos_id), None)
 
-        if len(list_video) == 0 :
+        if list_video is None:
             print(f'There is no {videos_id} found -------')
             return False
             
-        dict_video : dict = list_video[0]
-
-        if videos_id == dict_video.get('id') :
-            print(f'``````Found {videos_id} on cache.``````')
-            return True
-        
+        self.displaySpesific(cache=list_video)
 
     def deleteCache(self, videos_id_del : str) :
         """
@@ -190,51 +219,35 @@ class myCache() :
         if look_cache_data == False :
             return print(f"There is no {videos_id_del} available in cache data.\nUnable to delete.")
 
-        with open(self.CACHE_PATH, mode='r') as file :
+        with open(self.CACHE_PATH, mode='r+') as file :
             youtube_cache_del = json.load(file)
 
-        len_youtube_cache = len(youtube_cache_del)
+            len_youtube_cache = (len(youtube_cache_del) - 1 )
+            for cache in range(len_youtube_cache) :
+                if youtube_cache_del[cache]['id'] == videos_id_del :
+                    del youtube_cache_del[cache]
 
-        for cache in range(len_youtube_cache) :
-            if youtube_cache_del[cache]['id'] == videos_id_del :
-                del youtube_cache_del[cache]
-
-        with open(self.CACHE_PATH, mode='w') as file :
+            file.seek(0) #position cursor to the beginning
+            file.truncate() #clear exisisting data
             json.dump(youtube_cache_del, file, indent=4)
 
         return print(f"{videos_id_del} cache have been deleted.")
 
-    def lookId(self, videos_id : str) :
+    def displayAllCache(self) :
         """
-            Look for spesific videos id and print to the terminal
+            Display all cache in dump.json.\n
+            Print(ID, channel, video title, video language , and video url)
         """
-        do_ext = self.doExist()
-        if self.doExist() == False :
-            return print('No data shown')
-
-        with open(self.CACHE_PATH, mode='r') as file :
-            youtube_cache = json.load(file)
-
-        list_video : list = [x for x in youtube_cache if x['id'] == videos_id]
-
-        if len(list_video) == 0 :
-            print(f'There is no {videos_id} found -------')
-            return False
-            
-        dict_video : dict = next(item for item in list_video if isinstance(item, dict))
-
-        if videos_id == dict_video.get('id') :
-            display_single_cache = self.displaySpesific(cache=dict_video)
-
-    def displayCache(self) :
         
-        do_ext = self.doExist()
-        if self.doExist() == False :
+        # Check wheter file does exist or not
+        if self.doesExist() is False :
             return print(f"There is no cache")
 
+        # Load cache data in dump.json which is list type
         with open(self.CACHE_PATH, mode='r') as file :
-            cache_data = json.load(file)
+            cache_data : list = json.load(file)
 
+        # Loop through list of cache_data and display only necessary part to the terminal
         for item in cache_data :
             video_id = item['id']
             video_channel = item['channel']
@@ -250,21 +263,29 @@ class myCache() :
             print("="*30, '\n')
 
     def displaySpesific(self, cache : dict = {}) :
-        do_ext = self.doExist()
-        if self.doExist() == False :
+        """
+            display spesific cache with given id
+        """
+
+        # Check wheter file does exist or not
+        if self.doesExist() is False :
             return print(f"There is no cache")
         
+        # Run code when len(cache) not equal to zero
         if len(cache) != 0 :
            
-            print('\n')
+            print('\n') 
+
+            # Loop throught dictionaries and retrieve all data.
             for key, value in cache.items() :
                 if key == 'video' :
-                    videos_details = value[0]
+                    videos_details = value[0] # Access only dictionary , in this case value is list type.
                     videos_title = videos_details.get('video_title')
                     videos_language = videos_details.get('video_language')
                     videos_url = videos_details.get('video_url')
                     videos_description = videos_details.get('video_description')
                     
+                    # Print all data to the terminal
                     print("="*40)
                     print(f"Title           :   {videos_title}")
                     print(f"Language        :   {videos_language}")
@@ -272,15 +293,20 @@ class myCache() :
                     print(f"Description     :   \n{videos_description}")
                     print("="*40)
                         
+                # Print other value to the screen
                 else :
                     print(value)
 
+        # return such print statement to the terminal
         else :
             return print(f"There is no such data in cache.")
 
 class manageFile() :
 
     def __init__(self) :
+        """
+            Display welcome message all possible action users can do.
+        """
         print(f"""
             1. Display Cache
             2. Look Spesific Cache
@@ -289,12 +315,15 @@ class manageFile() :
             X. Quit
         """)
 
-    def userAction(self) :
-        my_cache = myCache()
+    def userAction(self) -> None :
+        """
+            Take an input from user and run code within a statement
+        """
+        my_cache = cacheManager()
         ask_user : str = input('Do : ')
 
         if ask_user == '1' :
-            my_cache.displayCache()
+            my_cache.displayAllCache()
         elif ask_user == '2' :
             user_video_id = input("Video ID : ")
             my_cache.lookId(videos_id=user_video_id)
@@ -307,52 +336,62 @@ class manageFile() :
             print('Invaid Input')
 
 
-class handler() :
+class userInterface() :
     """
         Handle all user interactions.
     """
     
     def __init__(self) :
-        pass
-
-    def commandMessage(self) :
         """
-            List of all possible action user can do. From downloading youtube video, youtube audio, get metadata, write substitle, to managing files.
+            Display list of action user can do.
         """
-
         print(f"""
             Select actions :
             1. Download Youtube Video
             2. Download Youtube Audio
-            4. Download Youtube Substitle
-            3. Show Video Info
+            3. Download Youtube Substitle
+            4. Show Video Info
             5. Manage file (only cache data)
             
             X. Exit 
         """)
 
+        self.commandMessage()
+
+    def commandMessage(self) -> None:
+        """
+            List of all possible action user can do. From downloading youtube video, youtube audio, get metadata, write substitle, to managing files.
+        """
+
         user_choice : str = input("I choose : ")
-        myYoutube = youtubeVideo()
         myInfo = showInfo()
-        myFile = manageFile()
 
         if user_choice.lower() == 'x' :
             exit()
         
         if user_choice.lower() == '1' :
             user_urls : str = input('Urls : ')
+            myYoutube = youtubeVideo()
             myYoutube.downloadVideos(user_urls)
+
         elif user_choice.lower() == '2' :
             user_urls : str = input('Urls : ')
+            myYoutube = youtubeVideo()
             myYoutube.downloadAudios(user_urls)
+
         elif user_choice.lower() == '3' :
             user_urls : str = input('Urls : ')
+            myYoutube = youtubeVideo()
             myYoutube.downloadSubtitle(user_urls)
+
         elif user_choice.lower() == '4' :
             user_urls : str = input('Urls : ')
             myInfo.getMetaData(user_urls)
+
         elif user_choice.lower() == '5' :
+            myFile = manageFile()
             myFile.userAction()
+
         else :
             print('Invalid Input')
 
@@ -360,7 +399,7 @@ class welcome() :
 
     def __init__(self) :
         """
-            Show greeting to user, user have to answer Y/N in question to proceed the program.
+            Show welcome message to user, user have to answer Y/N in question to proceed the program.
         """
 
         print(f"""
@@ -380,22 +419,18 @@ class welcome() :
             if user input y, run commandMessage()
         """
 
-        if user_answer.lower() == 'n' :
-            print('Good Bye ..... :)')
-            exit()
-        elif user_answer.lower() == 'y' :
-            user_handler = handler()
-            user_handler.commandMessage()
-            
+        while user_answer.lower != 'n' or user_answer.lower() == 'n': 
+            if user_answer.lower() == 'n' :
+                print('Good Bye ..... :)')
+                break
+            elif user_answer.lower() == 'y' :
+                 userInterface()
         else :
             print('You entered wrong one.')
 
 
 if __name__ == "__main__" :
     welcome()
-    # tes = myCache()
-    # tes.deleteCache("KdoaiGTIBY4")
-
 
 # Youtube Links for testing purpose
 # https://www.youtube.com/watch?v=WIyTZDHuarQ&t=15ss (Veritasium)
