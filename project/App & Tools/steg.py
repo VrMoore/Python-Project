94595d35cbe752745833c9e14b1a1bac8aedc961
Andy <115085994+VrMoore@users.noreply.github.com>
2025-03-23 20:45:58 +0700

    updated steg.py
    Change given text input into binary

--- project/App & Tools/steg.py
+++ project/App & Tools/steg.py
@@ -1,6 +1,21 @@
 # Import library and package
 from PIL import Image
 
+class convertText :
+
+    def __init__(self) :
+        self.user_secret_text = input('Input your text : ')
+        self.convert_to_binary(user_secret_text=self.user_secret_text)
+
+    def convert_to_binary(self, user_secret_text : str) -> bin:
+        character_bin = []
+        
+        for i in user_secret_text :
+            char_unicode = ord(i)
+            character_bin.append(bin(char_unicode)[2:])
+        
+        print(character_bin)
+        return character_bin
 
 class userInterfaces :
 
@@ -19,7 +34,7 @@ class userInterfaces :
 
         while users_ans in ('y','n') :
             if users_ans == 'y' :
-                print('Continue....')
+                do_convert = convertText()
                 break
             else :
                 print('Stop.')
